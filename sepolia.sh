3b64845622071f631ed1fd16fe096522dac98735
emmtte <emmtte@users.noreply.github.com>
2025-03-26 19:55:20 +0100

    maj sur sepolia

---
+++ sepolia.sh
@@ -0,0 +1,8 @@
+cd backend
+npx hardhat clean
+#rm -rf ignition/deployments
+#rm -rf ignition/cache
+npx hardhat ignition deploy ignition/modules/ezdefi.js --network sepolia
+cd ..
+cp -r backend/ignition/deployments/chain-11155111/artifacts/EZdefi*.json frontend/contracts/
+cp -r backend/ignition/deployments/chain-11155111/deployed_addresses.json frontend/contracts/
