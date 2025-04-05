e278d15a4e3e19e9fbe9223e14aa3fe6e566bfdd
emmtte <emmtte@users.noreply.github.com>
2025-04-06 00:56:52 +0200

    custom sepolia

--- frontend/utils/wagmi.js
+++ frontend/utils/wagmi.js
@@ -31,7 +31,7 @@ const customSepolia = defineChain({
 export const config = getDefaultConfig({
     appName: 'EZdefi',
     projectId: 'd3f86633ab6d1114bb1c18f0fdfbf72c',
-    chains: [sepolia, hardhat],
+    chains: [customSepolia, hardhat],
     transports: {
       [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'),
     },
