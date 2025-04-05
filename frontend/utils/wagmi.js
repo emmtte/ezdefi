b3abcb1e641ea8b9f1ae5fbc76e615a16bbe7634
emmtte <emmtte@users.noreply.github.com>
2025-04-05 20:01:23 +0200

    changer le rpc

--- frontend/utils/wagmi.js
+++ frontend/utils/wagmi.js
@@ -5,17 +5,25 @@ import {
   sepolia,
   hardhat
 } from 'wagmi/chains';
-import { http } from 'wagmi';
+
+
+const sepoliaWithCustomRpc = {
+  ...sepolia,
+  rpcUrls: {
+    ...sepolia.rpcUrls,
+    default: {
+      http: ['https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV']
+    },
+    public: {
+      http: ['https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV']
+    }
+  }
+};
 
 
 export const config = getDefaultConfig({
     appName: 'EZdefi',
     projectId: 'd3f86633ab6d1114bb1c18f0fdfbf72c',
-    chains: [mainnet, sepolia, hardhat],
+    chains: [mainnet, sepoliaWithCustomRpc, hardhat],
     ssr: true, // If your dApp uses server side rendering (SSR)
-    transports: {
-      [mainnet.id]: http('https://eth-mainnet.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'),
-      [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'),
-      [hardhat.id]: http()
-    }
   });
