5c2699ae2c3fc132bd7a0f9c51b56dde324da953
emmtte <emmtte@users.noreply.github.com>
2025-04-05 20:29:48 +0200

    ajout de sepolia.js

--- frontend/utils/wagmi.js
+++ frontend/utils/wagmi.js
@@ -1,42 +1,14 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, hardhat } from 'wagmi/chains';
+import {
+  mainnet,
+  hardhat
+} from 'wagmi/chains';
+import { sepolia } from './sepolia.js';
 
-// Définition personnalisée de Sepolia
-const customSepolia = {
-  id: 11155111,
-  name: 'Sepolia',
-  network: 'sepolia',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'Sepolia Ether',
-    symbol: 'ETH',
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'],
-    },
-    public: {
-      http: ['https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'],
-    },
-  },
-  blockExplorers: {
-    etherscan: {
-      name: 'Etherscan',
-      url: 'https://sepolia.etherscan.io',
-    },
-    default: {
-      name: 'Etherscan',
-      url: 'https://sepolia.etherscan.io',
-    },
-  },
-  testnet: true,
-};
-
-// Utilisation simple de la configuration par défaut sans transports personnalisés
 export const config = getDefaultConfig({
-  appName: 'EZdefi',
-  projectId: 'd3f86633ab6d1114bb1c18f0fdfbf72c',
-  chains: [mainnet, customSepolia, hardhat],
-  ssr: true,
-});
+    appName: 'EZdefi',
+    projectId: 'd3f86633ab6d1114bb1c18f0fdfbf72c',
+    chains: [mainnet, sepolia, hardhat],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+  });
