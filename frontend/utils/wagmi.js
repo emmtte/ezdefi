bdf6bdb5aa2b16052f2ebf500080aaf3e3507e93
emmtte <emmtte@users.noreply.github.com>
2025-04-05 20:53:02 +0200

    refait wagmi.js

--- frontend/utils/wagmi.js
+++ frontend/utils/wagmi.js
@@ -4,10 +4,16 @@ import {
   hardhat
 } from 'wagmi/chains';
 import { sepolia } from './sepolia.js';
+import { http } from 'viem';
+
 
 export const config = getDefaultConfig({
     appName: 'EZdefi',
     projectId: 'd3f86633ab6d1114bb1c18f0fdfbf72c',
     chains: [sepolia, hardhat],
     ssr: true, // If your dApp uses server side rendering (SSR)
+    transports: {
+      [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'),
+      [hardhat.id]: http(),
+    }
   });
