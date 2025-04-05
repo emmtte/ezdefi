33bdd7cc95873d2994367f8cee5efe419dc98825
emmtte <emmtte@users.noreply.github.com>
2025-04-05 19:39:18 +0200

    transport sepolia

--- frontend/utils/wagmi.js
+++ frontend/utils/wagmi.js
@@ -11,5 +11,8 @@ export const config = getDefaultConfig({
     appName: 'EZdefi',
     projectId: 'd3f86633ab6d1114bb1c18f0fdfbf72c',
     chains: [mainnet, sepolia, hardhat],
+    transports: {
+      [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV') },
     ssr: true, // If your dApp uses server side rendering (SSR)
   });
+
