21ba8151e68f1e25a17db978767691a873818d8f
emmtte <emmtte@users.noreply.github.com>
2025-04-05 19:54:06 +0200

    import http de wagmi

--- frontend/utils/wagmi.js
+++ frontend/utils/wagmi.js
@@ -5,20 +5,17 @@ import {
   sepolia,
   hardhat
 } from 'wagmi/chains';
+import { http } from 'wagmi';
 
-//2 nouvelles lignes
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
 
 export const config = getDefaultConfig({
     appName: 'EZdefi',
     projectId: 'd3f86633ab6d1114bb1c18f0fdfbf72c',
     chains: [mainnet, sepolia, hardhat],
     ssr: true, // If your dApp uses server side rendering (SSR)
-  //1 nouvelle ligne
-    providers: [
-    alchemyProvider({ apiKey: 'VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV' }),
-    publicProvider()
-  ]
-});
-  
+    transports: {
+      [mainnet.id]: http('https://eth-mainnet.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'),
+      [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'),
+      [hardhat.id]: http()
+    }
+  });
