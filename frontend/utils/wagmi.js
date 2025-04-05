7e8e700ec501ea50f2e786a73a85645d2ba380f8
emmtte <emmtte@users.noreply.github.com>
2025-04-05 20:11:54 +0200

    refait wagmi.js

--- frontend/utils/wagmi.js
+++ frontend/utils/wagmi.js
@@ -1,29 +1,48 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import {
-  mainnet,
-  sepolia,
-  hardhat
-} from 'wagmi/chains';
+import { mainnet, hardhat } from 'wagmi/chains';
+import { defineChain } from 'viem';
+import { http } from 'viem/transport';
 
-
-const sepoliaWithCustomRpc = {
-  ...sepolia,
+// Définition personnalisée de Sepolia
+const customSepolia = defineChain({
+  id: 11155111,
+  name: 'Sepolia',
+  network: 'sepolia',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Sepolia Ether',
+    symbol: 'ETH',
+  },
   rpcUrls: {
-    ...sepolia.rpcUrls,
     default: {
-      http: ['https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV']
+      http: ['https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'],
     },
     public: {
-      http: ['https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV']
-    }
-  }
-};
-
+      http: ['https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'],
+    },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'Etherscan',
+      url: 'https://sepolia.etherscan.io',
+    },
+    default: {
+      name: 'Etherscan',
+      url: 'https://sepolia.etherscan.io',
+    },
+  },
+  testnet: true,
+});
 
 export const config = getDefaultConfig({
-    appName: 'EZdefi',
-    projectId: 'd3f86633ab6d1114bb1c18f0fdfbf72c',
-    chains: [mainnet, sepoliaWithCustomRpc, hardhat],
-    ssr: true, // If your dApp uses server side rendering (SSR)
-  });
+  appName: 'EZdefi',
+  projectId: 'd3f86633ab6d1114bb1c18f0fdfbf72c',
+  chains: [mainnet, customSepolia, hardhat],
+  ssr: true,
+  transports: {
+    [mainnet.id]: http(),
+    [customSepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'),
+    [hardhat.id]: http(),
+  }
+});
