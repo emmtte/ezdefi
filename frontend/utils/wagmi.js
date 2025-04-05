06a5ee9b4672fd07098b01b63101acb100eaac16
emmtte <emmtte@users.noreply.github.com>
2025-04-05 20:15:17 +0200

    update wagmi.js

--- frontend/utils/wagmi.js
+++ frontend/utils/wagmi.js
@@ -1,11 +1,9 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { mainnet, hardhat } from 'wagmi/chains';
-import { defineChain } from 'viem';
-import { http } from 'viem/transport';
 
 // Définition personnalisée de Sepolia
-const customSepolia = defineChain({
+const customSepolia = {
   id: 11155111,
   name: 'Sepolia',
   network: 'sepolia',
@@ -33,16 +31,12 @@ const customSepolia = defineChain({
     },
   },
   testnet: true,
-});
+};
 
+// Utilisation simple de la configuration par défaut sans transports personnalisés
 export const config = getDefaultConfig({
   appName: 'EZdefi',
   projectId: 'd3f86633ab6d1114bb1c18f0fdfbf72c',
   chains: [mainnet, customSepolia, hardhat],
   ssr: true,
-  transports: {
-    [mainnet.id]: http(),
-    [customSepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/VXvc9IbtUntO_27RgSW1xxOvNIpZh5zV'),
-    [hardhat.id]: http(),
-  }
 });
